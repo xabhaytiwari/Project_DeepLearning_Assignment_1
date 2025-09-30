449ec498797266bebe3af0d384b673d7b8dc02ea
Abhay Tiwari <emailatabhay@gmail.com>
2025-09-30 12:24:08 +0530

    {rename} Question Ordering

--- question_15.py
+++ question_15.py
@@ -1,28 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 
 def model(y, t):
-    return (math.exp(t) + math.pow(t, 4) + math.sin(math.radians(t))) * t
+    return y * t
 
 t0 = 0
 y0 = 1
+
 h = 0.1
 t_end = 2
 
 t = np.arange(t0, t_end + h, h)
-y_rk2 = np.zeros(len(t))
-y_rk2[0] = y0
+y = np.zeros(len(t))
+
+y[0] = y0 
 
 for i in range(len(t) - 1):
-    k1 = h * model(y_rk2[i], t[i])
-    k2 = h * model(y_rk2[i] + k1/2, t[i] + h/2)
-    y_rk2[i + 1] = y_rk2[i] + k2
+    y[i + 1] = y[i] + h * model(y[i], t[i])
+print(y)
 
-plt.figure(figsize=(10, 8))
-plt.plot(t, y_rk2, 'r.--', label = 'RK-2 Approximation')
-plt.xlabel('Time (t)')
+plt.figure(figsize=(8, 6))
+plt.plot(t, y, 'r.--', label='Euler Approximation')
+plt.xlabel(r'Time ($t$)')
 plt.ylabel(r'$y(t)$')
+plt.title('Euler method Approximation')
 plt.legend()
 plt.grid(True)
 plt.show()
