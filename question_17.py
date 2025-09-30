27902a1b9436f3442a285b4020789bbd3bb19cd9
Abhay Tiwari <emailatabhay@gmail.com>
2025-09-30 12:27:47 +0530

    {solved} Question 17

---
+++ question_17.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def model(y, t):
+    return (t**2 + 1) * y 
+
+t0 = 0
+y0 = 1
+h = 0.1
+t_end = 2
+
+t = np.arange(t0, t_end + h, h)
+y_rk4 = np.zeros(len(t))
+y_rk4[0] = y0
+
+for i in range(len(t) - 1):
+    # Calculate k1 slope (slope at the start)
+    k1 = h * model(y_rk4[i], t[i])
+
+    # Calculate k2 & k3 slopes (slope at the midpoint)
+    k2 = h * model(y_rk4[i] + 0.5 * k1, t[i] + 0.5 * h)
+    k3 = h * model(y_rk4[i] + 0.5 * k2, t[i] + 0.5 * h)
+
+    # Calculate k4; the slope at end
+    k4 = h * model(y_rk4[i] + k3, t[i] + h)
+
+    # Calculate the next y value
+    y_rk4[i + 1] = y_rk4[i] + 1/6 * ( k1 + 2 * k2 + 2 * k3 + k4)
+
+plt.figure(figsize=(10, 8))
+plt.plot(t, y_rk4, 'r.--', label = 'RK-4 Approximation')
+# Plot the true solution for comparison 
+plt.xlabel('Time (t)')
+plt.ylabel(r'$y(t)$')
+plt.legend()
+plt.grid(True)
+plt.show()
